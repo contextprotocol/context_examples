f9ecaee1c6d117db29b9b2538cff7cacaab4684b
Alex Puig <cryptobenkei@protonmail.com>
2024-02-22 12:47:38 +0100

    Update package.json and register.ts

--- src/register.ts
+++ src/register.ts
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // Main : Test context.
-async function main() {
+async function main(ctxName: string) {
     const context: Context = new Context({
         connection: Connection.MUMBAI,
         rpcProviderUrl: process.env.RPC_PROVIDER
@@ -17,11 +17,15 @@ async function main() {
     });
     const wallet: ContextWallet = new ContextWallet(context, process.env.OWNER_PRIVKEY);
 
+    const walletTo: ContextWallet = new ContextWallet(context, "0xc922cf151a2b013fe170d9b0229ab0e979016aa79f39bffa99885fb62e9233d1");
+    console.log(walletTo.address);
+    console.log(walletTo.privateKey);
+
     // Write first commit.
-    console.log('Registering sergifern...');
-    const ctx = await context.clone('context', { storage, wallet });
-    const hash = await context.init('events1', wallet.address, 10, ctx);
+    console.log(`Registering ${ctxName} ...`);
+        const ctx = await context.clone('context', { storage, wallet });
+        const hash = await context.init(ctxName, walletTo.address, 10, ctx);
     console.log('Result:' + hash);
 }
 
-main().catch((error) => { console.error(error); });  
+main(process.argv[2]).catch((error) => { console.error(error); }); 
